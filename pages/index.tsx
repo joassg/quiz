fac594b7e35a2df31ba0b113aafb522be48dbd27
Joás de Souza Gomes <joassg@users.noreply.github.com>
2022-04-21 11:09:27 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
 //const BASE_URL = 'http://localhost:3000/api'
-const BASE_URL = 'https://quiz-xi-azure.vercel.app/'
+const BASE_URL = 'https://quiz-git-master-joassg.vercel.app//'
 
 export default function Home() {
   const router = useRouter()
